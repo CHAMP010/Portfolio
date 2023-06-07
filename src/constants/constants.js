62dd8507cd61ece5a6a213306b6b6f9249a068c9
Yash Soni <yashpc09@gmail.com>
2023-06-07 17:57:43 +0530

    project added

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,19 +1,27 @@
 export const projects = [
   {
+    title: 'roadmapAI',
+    description: "Using React with NextJS developed an app. The App is called roadmapAI and it is a simple roadmap generating app that allows users to generate roadmaps using AI.",
+    image: '/images/0.png',
+    tags: ['ReactJS', 'NextJS', 'TailwindCSS'],
+    visit: 'https://roadmapAI.vercel.app',
+    source: 'https://github.com/CHAMP010/karlo.github.io',
+    id: 0,
+  },
+  {
     title: 'Trello Clone',
     description: "Using Pure HTML, CSS and JS developed an app. The App is called Karlo and it is a simple Task management app that allows users to track their tasks and complete those tasks efficiently.",
     image: '/images/1.png',
     tags: ['HTML', 'CSS', 'JS'],
     visit: 'https://champ010.github.io/karlo.github.io/',
-    source: 'https://github.com/CHAMP010/karlo.github.io',
-    id: 0,
+    id: 1,
   },
   {
     title: 'CRM Admin',
     description: "A React application for businesses to help with Customer Relationship Management. The app contains advance UI elements like dropdown, profile, analytics etc. It allows businesses to keep track of their progress through analytical overview",
     image: '/images/2.png',
     tags: ['React', 'JavaScript', 'CSS', 'Node'],
-    id: 1,
+    id: 2,
   },
   {
     title: 'CHAMP-Keep',
@@ -22,21 +30,21 @@ export const projects = [
     tags: ['HTML', 'CSS', 'JS'],
     source: 'https://github.com/CHAMP010/keep.github.io',
     visit: 'https://champ010.github.io/keep.github.io',
-    id: 2,
+    id: 3,
   },
   {
     title: 'Book Store',
     description: "The app is developed as Book Store using ReactJS. It is a client project done by me. It is a simple frontend with some functionalities like search book, add to cart, login etc.",
     image: '/images/4.png',
     tags: ['React', 'Node', 'CSS', 'Mongo'],
-    id: 3,
+    id: 4,
   },
   {
     title: 'To-Do List',
     description: "To-Do List App Developed using HTML, JS and CSS with simple UI which is easy to navigate",
     image: '/images/5.png',
     tags: ['HTML', 'CSS', 'JS'],
-    id: 4,
+    id: 5,
   },
   {
     title: 'Food App',
@@ -44,7 +52,7 @@ export const projects = [
     image: '/images/6.png',
     tags: ['React', 'CSS', 'Node', 'Mongo', 'Socket.io'],
     visit: 'https://onesta-patan.herokuapp.com/',
-    id: 4,
+    id: 6,
   },
 ];
 
